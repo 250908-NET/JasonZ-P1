a01cddda4c493c3461197d319865e3630f387728
alt-potato <jason.why.zhuang@gmail.com>
2025-09-26 03:29:51 -0400

    Increase name field lengths

---
+++ backend/CardDeck.Api/Migrations/20250926072313_IncreaseNameLengths.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CardDeck.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseNameLengths : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Suits",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedAt",
+                table: "Cards",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValueSql: "SYSDATETIMEOFFSET()",
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rank",
+                table: "Cards",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldMaxLength: 5);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Suits",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(32)",
+                oldMaxLength: 32);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedAt",
+                table: "Cards",
+                type: "datetimeoffset",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldDefaultValueSql: "SYSDATETIMEOFFSET()");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rank",
+                table: "Cards",
+                type: "nvarchar(5)",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(16)",
+                oldMaxLength: 16);
+        }
+    }
+}
